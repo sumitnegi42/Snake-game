93cb3534aaf75fcf37e40fb23ae82b98296c26cb
sumitnegi41 <104970758+sumitnegi42@users.noreply.github.com>
2023-02-16 13:13:48 +0530

    second commit

--- js/index.js
+++ js/index.js
@@ -31,6 +31,9 @@ function isCollide(snake) {
             return true;
         }
     }
+    if(snake[0].x >=18 || snake[0].x <=0 && snake[0].y >=18 || snake[0].y <=0){
+        return true
+    }
         
     return false;
 }
